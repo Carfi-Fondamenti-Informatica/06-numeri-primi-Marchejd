9dc6999e1df443418dd682b8ff0715f4e4c23eeb
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-01 18:43:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
 int main(){
+   int n = 0, i=1;
+        cin>>n;
+if (primo(n,i)==true)
+{
+    cout<<"è primo"<<endl;
+}
+if (primo(n,i)==false)
+{
+    cout<<"non è primo"<<endl;
+}
+  
   return 0;
 }
