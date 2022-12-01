b44a6f566206767b2dc9e1ad1ed6abe33e154469
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-01 18:44:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
 
+bool primo (int n, int i)
+{
+
+    while (i<n)
+    {i++;
+        if (n % i == 0 && n == i)
+        {
+            return true;
+        }
+        if (n % i == 0 && n != i )
+        {
+            return false;
+        }
+        if (n%i!=0)
+        {
+            primo (n, i);
+        }
+    }
+
+    }
